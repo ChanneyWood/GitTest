5eb7b236d948d66fe31ce83c86c870833099159c
liliang <1257947660@qq.com>
2019-07-14 16:25:32 +0800

    改一下仓库名字，新建一个类

---
+++ app/src/main/java/com/example/a12579/learnservice/UpdateReposiName.java
@@ -0,0 +1,11 @@
+package com.example.a12579.learnservice;
+
+/**
+ * Created by 12579 on 2019/7/14.
+ */
+
+public class UpdateReposiName {
+    public static void main(String[] args) {
+        System.out.println("update name");
+    }
+}
